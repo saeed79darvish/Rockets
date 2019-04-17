9793c62ef3949a9a511cd3cf2bb5dada3627cb1a
saeed79darvish <s79darvish@gmail.com>
2019-04-17 00:31:49 -0700

    add manifest and style to client side

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,19 +1,37 @@
-import React from 'react';
+import React, {Fragment} from 'react';
 import Logo from './logo.png'
+import ApolloClient from "apollo-boost";
+import { ApolloProvider } from "react-apollo";
+import Launches from './launches.jsx';
+import './App.css';
+
+const client = new ApolloClient({
+    uri: 'http://localhost:5000/graphql'
+})
 
 
 class App extends React.Component {
 
     render() {
-        return (<div>
-            <img src={Logo} style={{
-                width: 400,
-                display: 'block',
-                margin: 'auto',
-                padding: '10px'
-            }} />
+        return (
+            <ApolloProvider client={client}>
+                
+                <div className="container">
+                    <img src={Logo} style={{
+                       
+                        width: 400,
+                        display: 'block',
+                        margin: 'auto'
+                      
+                    }} />
 
-        </div>);
+                </div>
+                <div className="App">
+                <Launches />
+                   </div>
+                  
+                    
+            </ApolloProvider>);
     }
 }
 
--- db/index.js
+++ db/index.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const graphqlHTTP = require('express-graphql');
-const schema= require('./schema.js')
+const schema = require('./schema.js')
+const cors = require('cors');
 
 const app = express();
+
+app.use(cors());
 
 
 
